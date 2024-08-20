1963340f906b2176ed49c9875ef1d1eb61e12826
AccidentByDragon <karl17loe@hotmail.com>
2024-08-20 11:09:03 +0200

    board reset
    tester och kod för att starta om spelet via propmt kommandon

---
+++ classes/App.js
@@ -0,0 +1,25 @@
+import prompt from '../helpers/prompt.js'
+import Board from './Board.js'
+
+export default class App {
+  constructor() {
+    this.keepPlaying = true;
+    while (keepPlaying === true) {
+      this.board = new Board()
+      //resten av appens logik ska vara här
+      this.keepPlaying = this.boardReset()
+    }
+  }
+
+
+boardReset() {
+  let playAgain = prompt('Vill ni spela igen? (ja/nej)? ');
+  if (playAgain !== 'ja')
+    {
+      return false;
+  }
+  else {
+    return true
+  }
+  }
+}
---
+++ tests/BoardReset.test.js
@@ -0,0 +1,43 @@
+import { expect, test } from "vitest";
+import {
+  promptQuestions,
+  consoleOutput,
+  setMockAnswers,
+  log
+} from './helpers/mockPromptAndConsoleLog.js';
+import App from "../classes/App.js";
+
+test('The Board should ask if the player wishes to restart'), () => {
+  // setting mocked answers
+  setMockAnswers('nej');
+  // checking program asks the correct questions
+  expect(promptQuestions[0]).toBe('Vill ni spela igen? (ja/nej)? ')
+}
+
+test('The Board should reset itself if the player wishes to play again'), () => {
+  // setting mcoked answers
+  setMockAnswers('ja');
+  // create and instance of board OBS this may need to be App or somethign else instead
+  let app = new App();
+  // checking program asks the correct questions
+  expect(promptQuestions[0]).toBe('Vill ni spela igen? (ja/nej)? ')
+  // when answered yes the board should reset and be clear
+  expect(app.board.matrix).toEqual([
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  ]);
+}
+
+test('The Board should not change from its final state id the Player does not want to restart'), () => {
+  // setting mcoked answers
+  setMockAnswers('nej');
+  // create and instance of board OBS this may need to be App or somethign else instead
+  let app = new App();
+  // checking program asks the correct questions
+  expect(promptQuestions[0]).toBe('Vill ni spela igen? (ja/nej)? ');
+  expect(app.keepPlaying).toBe(false);
+}
