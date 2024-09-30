59ef7c909aaef4affb1876fcb4a0f8e6a42aa4d4
Jensju <jens@modulmedia.se>
2024-09-30 11:45:52 +0200

    iframes

---
+++ tests/iframeTry.test.js
@@ -0,0 +1,4 @@
+import { expect, test } from 'vitest';
+import click from './helpers/mock-help/triggerOnclick.js';
+import sleep from './helpers/mock-help/sleep.js';
+import regPlayHumanStart from './helpers/common tasks/registerPlayersHumanvsBot.js';
