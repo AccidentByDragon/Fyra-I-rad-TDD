200db7eb8b446fb53c43f64332a58bb24d8455b5
Jensju <jens@modulmedia.se>
2024-10-01 15:22:52 +0200

    Styletag setup

--- tests/iframeTry.test.js
+++
@@ -1,4 +0,0 @@
-import { expect, test } from 'vitest';
-import click from './helpers/mock-help/triggerOnclick.js';
-import sleep from './helpers/mock-help/sleep.js';
-import regPlayHumanStart from './helpers/common tasks/registerPlayersHumanvsBot.js';
