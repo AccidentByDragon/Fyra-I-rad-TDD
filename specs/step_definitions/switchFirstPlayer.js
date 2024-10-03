6e729f9da94a402205953b254bc8eb575b3afc42
AlexandraFilipsson <alexandrafilipsson@gmail.com>
2024-10-02 20:03:17 -0500

    added new feature
    but test still doesn´t work

--- specs/step_definitions/switchFirstPlayer.js
+++ specs/step_definitions/switchFirstPlayer.js
@@ -1,21 +1,20 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 import { getIframeBody } from '../helpers/iframe.js';
 
-Given('game is tarted by joining the nerwrok', () => {
+// Scenario 1: Play a full game
 
-  // visit the 'helper' we set up with two iframes
-  // where each iframe emulates one player in a network
+Given('the game is started by joining the network', () => {
   cy.visit('/iframed-network-play.html');
 
-  // player X - first player - start network game and get code
+  // Player X (Red) starts the game
   getIframeBody('iframe#Red').find('.button.Yes').click();
   getIframeBody('iframe#Red').find('.button.Create').click();
   getIframeBody('iframe#Red').find('input[name="answer"]').type('Anna{enter}');
-  getIframeBody('iframe#Red').find('input[name="joinCode"]').then(element => {
-    // we have the join code
-    let joinCode = element.val();
 
-    // player O - second player join the game
+  // Get the join code for Player O (Yellow)
+  getIframeBody('iframe#Red').find('input[name="joinCode"]').then(element => {
+    let joinCode = element.val();
+    // Player O (Yellow) joins the game
     getIframeBody('iframe#Yellow').find('.button.Yes').click();
     getIframeBody('iframe#Yellow').find('.button.Join').click();
     getIframeBody('iframe#Yellow').find('input[name="answer"]').type('Beata{enter}');
@@ -24,32 +23,56 @@ Given('game is tarted by joining the nerwrok', () => {
   });
 });
 
-Then('players make their moves till someone has won', () => {
+When('the players make their moves until one player wins', () => {
+  // Close any visible dialog box if present before starting the game
+  getIframeBody('iframe#Red').find('dialog').then(dialog => {
+    if (dialog.is(':visible')) {
+      dialog.find('button').click(); // Close the dialog if it's open
+    }
+  });
 
-  cy.wait(4000);  // Wait for UI update
-  getIframeBody('iframe#Red').find('.cell:nth-child(39)').should('exist').click();
-  cy.wait(4000);  // Wait for UI update
-  getIframeBody('iframe#Yellow').find('.cell:nth-child(38)').should('exist').click();
-  cy.wait(4000);
-  getIframeBody('iframe#Red').find('.cell:nth-child(32)').should('exist').click();
-  cy.wait(4000);  // Wait for UI update
-  getIframeBody('iframe#Yellow').find('.cell:nth-child(37)').should('exist').click();
-  cy.wait(4000)
-  getIframeBody('iframe#Red').find('.cell:nth-child(25)').should('exist').click();
-  cy.wait(4000);  // Wait for UI upda
+  // Simulate moves for both players until Player X (Red) wins
+  const moves = [
+    { player: 'Red', cell: 39 },
+    { player: 'Yellow', cell: 38 },
+    { player: 'Red', cell: 32 },
+    { player: 'Yellow', cell: 37 },
+    { player: 'Red', cell: 25 },
+    { player: 'Yellow', cell: 36 },
+    { player: 'Red', cell: 18 }  // Winning move
+  ];
 
+  moves.forEach(({ player, cell }, index) => {
+    cy.wait(1000); // Reduced wait time to speed up the test
+    getIframeBody(`iframe#${player}`).find(`.cell:nth-child(${cell})`).click({ force: true }); // Use force to bypass overlay issues
+  });
 
-  getIframeBody('iframe#Yellow').find('.cell:nth-child(36)').should('exist').click();
-  cy.wait(4000)
-  getIframeBody('iframe#Red').find('.cell:nth-child(18)').should('exist').click();
-  cy.wait(4000);  // Wait for UI update
-  getIframeBody('iframe#Yellow').find('.cell:nth-child(36)').should('exist').click();
-
+  // Adding an extra wait after the winning move to allow for UI updates
+  cy.wait(2000);
 });
 
+Then('the winning message should be visible for both players', () => {
+  // Ensure the winning message appears
+  cy.wait(2000); // Wait for the message to render
+  getIframeBody('iframe#Red').find('p:contains("Red: Anna won!")').should('be.visible');
+  getIframeBody('iframe#Yellow').find('p:contains("Red: Anna won!")').should('be.visible');
+});
 
+// Scenario 2: Switch first player
 
-Then('winning message should be wisible for both players, try to check sound as well', () => {
-  getIframeBody('iframe#Red').find('p:contains("Red: Anna won!")');
-  getIframeBody('iframe#Yellow').find('p:contains("Red: Anna won!")');
+Given('that the game has ended', () => {
+  // Ensure that the game has ended
+  getIframeBody('iframe#Red').find('p:contains("Red: Anna won!")').should('be.visible');
+  getIframeBody('iframe#Yellow').find('p:contains("Red: Anna won!")').should('be.visible');
+});
+
+When('the players choose to play again', () => {
+  // Ensure the "Play Again" button is visible, then click it for both players
+  getIframeBody('iframe#Red').find('.button.PlayAgain', { timeout: 10000 }).should('be.visible').click();
+  getIframeBody('iframe#Yellow').find('.button.PlayAgain', { timeout: 10000 }).should('be.visible').click();
+});
+
+Then('the second player from the previous game should become the first player in the new game', () => {
+  // Verify that Player O (Yellow) becomes the first player in the new game
+  getIframeBody('iframe#Yellow').find('p:contains("Yellow: Beata\'s turn...")').should('be.visible');
 });
--- specs/switchFirstPlayer.feature
+++ specs/switchFirstPlayer.feature
@@ -1,9 +1,11 @@
 Feature: Switch first player when clicking "Play again"
 
-  Scenario: Start a game
-    Given that game is played bla bla bla
+  Scenario: Play a full game
+    Given the game is started by joining the network
+    When the players make their moves until one player wins
+    Then the winning message should be visible for both players
 
   Scenario: The second player becomes the first player in the next game
-    Given that a game has ended
+    Given that the game has ended
     When the players choose to play again
     Then the second player from the previous game should become the first player in the new game
