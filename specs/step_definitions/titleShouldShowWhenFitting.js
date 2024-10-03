e911c4ab579b64b5d1409b9c0fbe2fa57402dc8a
AccidentByDragon <karl17loe@hotmail.com>
2024-10-03 13:12:36 +0200

    background

--- specs/step_definitions/titleShouldShowWhenFitting.js
+++ specs/step_definitions/titleShouldShowWhenFitting.js
@@ -1,5 +1,24 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
+import { getIframeBody } from '../helpers/iframe.js';
 
 Given('We have started a game', () => {
-  // TODO: implement step
+  // visit the 'helper' we set up with two iframes
+  // where each iframe emulates one player in a network
+  cy.visit('/iframed-network-play.html');
+
+  // player X - first player - start network game and get code
+  getIframeBody('iframe#Red').find('.button.Yes').click();
+  getIframeBody('iframe#Red').find('.button.Create').click();
+  getIframeBody('iframe#Red').find('input[name="answer"]').type('Anna{enter}');
+  getIframeBody('iframe#Red').find('input[name="joinCode"]').then(element => {
+    // we have the join code
+    let joinCode = element.val();
+
+    // player O - second player join the game
+    getIframeBody('iframe#Yellow').find('.button.Yes').click();
+    getIframeBody('iframe#Yellow').find('.button.Join').click();
+    getIframeBody('iframe#Yellow').find('input[name="answer"]').type('Beata{enter}');
+    getIframeBody('iframe#Yellow').find('dialog:contains("join code") input[name="answer"]')
+      .type(joinCode + '{enter}');
+  });
 });
